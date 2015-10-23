7eb1038b6c1a4b91cc7b620f577de95179b5fa0e
Avid Maulana Saputra <avidmaulanas@gmail.com>
2015-10-23 22:51:36 +0700

    Fixed test Cucumber

--- features/step_definitions/edit_blog_steps.rb
+++ features/step_definitions/edit_blog_steps.rb
@@ -2,16 +2,6 @@ Given /^there is a post with title "(.*?)" and\tcontent "(.*?)"$/ do |title, con
 	@post = create(:post, title: title, content: content)
 end
 
-When /^I edit this post$/ do
-	visit(edit_post_path @post)
-end
-
-When /^I update title to "(.*?)" and content to "(.*?)"$/ do |title, content|
-	@updated_title = title
-	@updated_content = content
-	@post.update_attributes!({ title: @updated_title, content: @updated_content })
-end
-
 Then /^I can see it has been updated$/ do
 	step %{I am on the blog homepage}
 	expect(find(:css, "table tr:nth-child(2)")).to have_content(@updated_title)
